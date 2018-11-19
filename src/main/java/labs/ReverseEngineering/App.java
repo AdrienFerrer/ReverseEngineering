d5c10dcf07e617814945fa00da7820173fc88d75
AdrienFerrer <adrien.ferrer@isep.fr>
2018-11-19 11:52:35 +0100

    tables

--- src/main/java/labs/ReverseEngineering/App.java
+++ src/main/java/labs/ReverseEngineering/App.java
@@ -15,7 +15,6 @@ public class App {
 	public static void main(String[] args) {
 		Connection con = new dbconnect().getConnection();
 		System.out.println(getDatabaseTablesNames(con));
-		System.out.println(getDatabaseTableColumns(con,"actor"));
 	}
 
 	public static LinkedList<String> getDatabaseTablesNames(Connection con) {
@@ -33,42 +32,4 @@ public class App {
 		}
 		return tableNames;
 	}
-
-	
-	public static Map<String, Integer> getDatabaseTableColumns(Connection con, String table) {
-		Map<String, Integer> infos  = new HashMap<String, Integer>();
-		try {
-			DatabaseMetaData dbmd = con.getMetaData();
-			ResultSet rs = dbmd.getColumns(null, null, table, null);
-			while (rs.next()) {
-				infos.put(rs.getString("COLUMN_NAME"), rs.getInt("DATA_TYPE"));
-			}
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return infos;
-	}
-	
-	public static Map<String,String,String,String,String> getKeys (Connection con,String table) {
-		try {
-			DatabaseMetaData dbmd = con.getMetaData();
-			ResultSet rs = dbmd.getImportedKeys(null, null, table);
-			while (rs.next()) {
-				infos.put(rs.getString("COLUMN_NAME"), rs.getInt("DATA_TYPE"));
-			}
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return infos;
-		
-	}
-	
-	
-	
-	
-	
-	
-	
 }
