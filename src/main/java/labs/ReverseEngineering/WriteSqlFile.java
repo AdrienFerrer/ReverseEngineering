7135ab010c1e3363e87b32b34bc12cf3fac630f5
Pierre Goffic <pierre.goffic@isep.fr>
2018-11-21 23:08:28 +0100

    WriteSqlFile class

---
+++ src/main/java/labs/ReverseEngineering/WriteSqlFile.java
@@ -0,0 +1,54 @@
+package labs.ReverseEngineering;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteSqlFile {
+	private String query;
+	private String pathFile;
+	private String extension;
+
+	public WriteSqlFile(String query, String pathFile, String extension) {
+		super();
+		this.query = query;
+		this.pathFile = pathFile;
+		this.extension = extension;
+	}
+
+	public void generateFile() {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(pathFile + extension));
+			writer.write(query);
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public String getQuery() {
+		return query;
+	}
+
+	public void setQuery(String query) {
+		this.query = query;
+	}
+
+	public String getPathFile() {
+		return pathFile;
+	}
+
+	public void setPathFile(String pathFile) {
+		this.pathFile = pathFile;
+	}
+
+	public String getExtension() {
+		return extension;
+	}
+
+	public void setExtension(String extension) {
+		this.extension = extension;
+	}
+
+}
