a32cb6d6c09e1e5e2cd1bad1e95d432a6fff63da
AdrienFerrer <adrien.ferrer@isep.fr>
2018-11-21 22:24:21 +0100

    debug

--- src/main/java/labs/ReverseEngineering/App.java
+++ src/main/java/labs/ReverseEngineering/App.java
@@ -29,12 +29,10 @@ public class App {
 	public static String sqlQuery (dbTables Tables) {
 		for(int i=0;i<Tables.getSize();i++) {
 			for(int j=0;j<Tables.getTables().size();j++) {
-				for(int k=0;j<Tables.getTables().get(j).getSize();j++) {
+				for(int k=0;j<Tables.getTables().get(j).getSize();k++) {
 					System.out.println(Tables.getTables().get(j).getAllColumns().get(k).getColumnName());
 				}
-
 			}
-			
 		}
 		
 		return "";
